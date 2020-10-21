20b3d548f6d5402b1d48ab96d4a112a478956fa6
Seldagarabedian <69519761+Seldagarabedian@users.noreply.github.com>
2020-10-21 23:54:06 +0200

    åtgärdat ett error

--- DAL/Repositories/IRepository.cs
+++ DAL/Repositories/IRepository.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 
 namespace DAL.Repositories
 {
-    public interface IRepository<T> where T:class
+    public interface IRepository<T> where T : class
     {
         void Add(T entity); //ska man även lägga till kategori och uppdateringsfrekvens som parametrar?
         void Delete(string valdKategori, string valdFeed);
-       // void DeleteKategori;//(vad ska den ta för parameter?)
+        // void DeleteKategori;//(vad ska den ta för parameter?)
         void Update(string URL, string nyttKategoriNamn, int uppdateringsFrekvens, string kategori); //stämmer parametrarna?
         void SaveChanges();
         List<T> GetAll();
+
+    }
     
 }
